4b2caa5e2042c1d651d7af40b32b85d994f5a888
liangjian <1738757@qq.com>
2017-05-22 21:20:19 +0800

    url 管理

--- urlmanager.py
+++ urlmanager.py
@@ -1,9 +1,46 @@
-
-
-
 class UrlManager:
-
-    def __init__(self):
-        self.uncrawllist = []
+    def __init__(self, name, collection, reportprocess, log):
         self.crawllist = []
         self.failcrawlist = []
+        self.name = name
+        self.curUrl = None
+        self.reportprocess = reportprocess
+        self.collection = collection
+        self.log = log
+
+    def initdata(self):
+        '''
+        读取要爬取的url
+        :return: 
+        '''
+        pass
+
+    def pushCrawlUrl(self, url):
+        i = self.crawllist.index(url)
+        if i == -1:
+            self.crawllist.append(url)
+            return True
+        else:
+            return False
+
+    def next(self):
+        length = len(self.crawllist)
+        if length > 0:
+            self.curUrl = self.crawllist.popleft()
+
+            if self.reportprocess:
+                self.log.print('index (%)-%s'.format(length, self.curUrl))
+
+            return self.curUrl
+        else:
+            raise StopIteration()
+
+    def cururlisfail(self):
+        self.failcrawlist.append(self.curUrl)
+
+    def done(self):
+        '''
+        保存状态
+        :return: 
+        '''
+        pass
