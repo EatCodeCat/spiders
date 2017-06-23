7deea5e9e02762bcfd4e2a281950bb626d7cd354
liangjian <1738757@qq.com>
2017-06-23 18:44:53 +0800

    任务管理后台

--- yellohub/aotuvideo.py
+++ yellohub/aotuvideo.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 __author__ = 'think'
 import minspider
-from yellohub.basecrawler import BaseCrawler
 from  collections import deque
 from mongodao.yellohubmodel import VideosModel
+from datetime import datetime
+import re
 
 
 class AotuVideoCrawler(minspider.Crawler):
@@ -13,14 +14,13 @@ class AotuVideoCrawler(minspider.Crawler):
         template_url = 'http://www.aotu17.com/recent/%s/'
 
         url_arr = ['http://www.aotu17.com/']
-        for i in range(2, 352):
+        for i in range(2, 3):
             url_arr.append(template_url % i)
 
         self.urlmanager = minspider.UrlManager(deque(url_arr))
         self.task_item = minspider.TaskItem()
         self.dao = VideosModel()
         super().__init__(self.urlmanager)
-
 
     def detail_parser(self, response):
         soup = self.response_txt_soup(response)
@@ -45,7 +45,6 @@ class AotuVideoCrawler(minspider.Crawler):
                 tagset.add(tag.string)
         self.item['tag_list'] = list(tagset)
 
-
     def parse(self, response):
         soup = super().parse(response)
         panel = soup.select('.panel-body')
@@ -64,13 +63,16 @@ class AotuVideoCrawler(minspider.Crawler):
                     'detail': detailUrl,
                     'title': title,
                     'thumb_pic': thumb_pic,
+                    'update_time': datetime.now()
                 }
                 print(detailUrl)
                 self.request(self.host + detailUrl, self.detail_parser)
                 self.items.append(self.item)
             return self.items
 
+
     def item_persistence(self, items):
         for item in items:
             self.dao.insert_one(item)
+
 
