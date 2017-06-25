99e842acff06d9204fb10ba7980f840c6b885d7c
EatCodeCat <17387575@qq.com>
2017-06-26 02:38:10 +0800

    爬取管理

--- mongodao/yellohubmodel.py
+++ mongodao/yellohubmodel.py
@@ -5,6 +5,7 @@ __author__ = 'think'
 from mongodao import mongoclient
 
 
+
 class VideosModel(mongoclient.MClient):
     def __init__(self):
         super().__init__('yellohub', 'videos')
--- test.py
+++ test.py
@@ -1,12 +1,25 @@
 from  mongodao.yellohubmodel import VideosModel
 from collections import Counter
+import jieba
+import jieba.analyse
 model = VideosModel()
+
+jieba.set_dictionary('dict.txt.big.txt')
 
 c = model.find()
 
 tag_list = []
+title_list = []
 
 for item in c:
-    tag_list += item['tag_list']
+    title_list.append(item['title'])
+    seg_list = jieba.cut(item['title'])
+    tag_list += seg_list
+    print(", ".join(seg_list))
+    #item['tag_list'] = [it for it in item['tag_list'] if it != '性爱']
+    #model.replace_one(item, **{'_id':item['_id']})
 
+
+#tags =jieba.analyse.extract_tags(''.join(title_list), topK=50,withWeight=True)
+Counter(tag_list)
 print(Counter(tag_list))
