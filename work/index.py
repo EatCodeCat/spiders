b6384dd2d70b4800206010666b7eb49aa06a4fc7
EatCodeCat <17387575@qq.com>
2017-07-01 15:32:45 +0800

    爬取管理

--- work/index.py
+++ work/index.py
@@ -245,7 +245,7 @@ def add_job(task_id):
     if job is None:
         task = fetch_one('select * from task where id=' + str(task_id))
         task = [task[1], task[4], task[0]]
-        scheduler.add_job('task_' + str(task_id), do_touzhuInfo_list, trigger='cron', hour=15, minute=30, args=task)
+        scheduler.add_job('task_' + str(task_id), do_touzhuInfo_list, trigger='cron', hour=15-8, minute=40, args=task)
 
 
 @app.route('/')
